64d89b02758c3a7328435f28988e6fd8c8075b47
ANUSH9 <loginanushgupta@gmail.com>
2022-05-31 19:27:54 +0530

    Add project files.

---
+++ ConsoleApp3/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Class1
+    {
+  
+        public void multi()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(i);
+            }
+        }
+        public void multi2()
+        {
+            for(int i =0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(i);
+            }
+        }
+        public void paral(string item)
+        {
+            Console.WriteLine(item);
+        }
+      
+        
+        
+        public void parall()
+        {
+            List<string> detaillist = new List<string>()
+            {
+                "apple","car","time","date"
+            };
+
+            /* for (int i = 0; i < detaillist.Count; i++)
+             {
+                 Console.WriteLine(i);
+             }*/
+            Parallel.ForEach(detaillist, item => paral(item));
+             
+        }
+        public void multi3()
+        {
+            Thread t = Thread.CurrentThread;
+            Console.WriteLine(t.Name + " is running");
+        }
+
+    }
+
+}
---
+++ ConsoleApp3/mainClass2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class mainClass2
+    {
+        static void Main()
+        {
+            Class1 obj = new Class1();
+            obj.parall();
+
+            Thread a = new Thread(obj.multi);
+            Thread b = new Thread(obj.multi2);
+            Thread c = new Thread(obj.parall);
+            Thread d = new Thread(obj.multi3);
+            Thread e = new Thread(obj.multi3);
+            c.Start();
+            //a.Start();
+            //b.Start();
+        }
+    }
+}
